b24cb218987f061ff260dc6ea5c6befdf4ffdd85
LeonardRodri <rodriguespeixoto.1995@gmail.com>
2020-09-16 15:13:07 -0300

    v2

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const path = require('path')
 const app = express() 
+const PORTA = process.env.PORT || 3000
 
-app.listen(3000, () =>{
+app.listen(PORTA, () =>{
     console.log("Deu certo! 3000 ")
 })
 
